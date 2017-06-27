3340a9e72a5127749cb168cdefcc0570a6c361d7
Nathaniel Cook <nvcook42@gmail.com>
2017-06-27 10:49:22 -0600

    update edge benchmark

--- edge_test.go
+++ edge_test.go
@@ -5,126 +5,135 @@ import (
 	"testing"
 	"time"
 
+	"github.com/influxdata/kapacitor/edge"
 	"github.com/influxdata/kapacitor/models"
 	"github.com/influxdata/kapacitor/pipeline"
 	"github.com/influxdata/kapacitor/services/logging/loggingtest"
 )
 
-func TestEdge_CollectPoint(t *testing.T) {
-	name := "point"
-	ls := loggingtest.New()
-	e := NewLegacyEdge(newEdge("BCollectPoint", "parent", "child", pipeline.StreamEdge, defaultEdgeBufferSize, ls))
-	p := models.Point{
-		Name: name,
-		Tags: models.Tags{
-			"tag1": "value1",
-			"tag2": "value2",
-			"tag3": "value3",
-			"tag4": "value4",
-		},
-		Group: models.ToGroupID(name, nil, models.Dimensions{}),
-		Fields: models.Fields{
-			"field1": 42,
-			"field2": 4.2,
-			"field3": 49,
-			"field4": 4.9,
-		},
-	}
+var name = "edge_test"
+var now = time.Now()
+var groupTags = models.Tags{
+	"tag1": "value1",
+	"tag2": "value2",
+}
+var groupDims = models.Dimensions{TagNames: []string{"tag1", "tag2"}}
+var groupID = models.ToGroupID(name, groupTags, groupDims)
 
-	e.CollectPoint(p)
-	np, ok := e.NextPoint()
+var point = edge.PointMessage{
+	Name: name,
+	Time: now,
+	Tags: models.Tags{
+		"tag1": "value1",
+		"tag2": "value2",
+		"tag3": "value3",
+		"tag4": "value4",
+	},
+	Group: groupID,
+	Fields: models.Fields{
+		"field1": 42,
+		"field2": 4.2,
+		"field3": 49,
+		"field4": 4.9,
+	},
+}
+
+var batch = edge.BufferedBatchMessage{
+	Begin: edge.BeginBatchMessage{
+		Name:     name,
+		Group:    groupID,
+		Tags:     groupTags,
+		SizeHint: 2,
+	},
+	End: edge.EndBatchMessage{
+		TMax: now,
+	},
+	Points: []edge.BatchPointMessage{
+		{
+			Time: now.Add(-1 * time.Second),
+			Tags: models.Tags{
+				"tag1": "value1",
+				"tag2": "value2",
+				"tag3": "first",
+				"tag4": "first",
+			},
+			Fields: models.Fields{
+				"field1": 42,
+				"field2": 4.2,
+				"field3": 49,
+				"field4": 4.9,
+			},
+		},
+		{
+			Time: now,
+			Tags: models.Tags{
+				"tag1": "value1",
+				"tag2": "value2",
+				"tag3": "second",
+				"tag4": "second",
+			},
+			Fields: models.Fields{
+				"field1": 42,
+				"field2": 4.2,
+				"field3": 49,
+				"field4": 4.9,
+			},
+		},
+	},
+}
+
+func TestEdge_CollectPoint(t *testing.T) {
+	ls := loggingtest.New()
+	e := newEdge("TestEdge_CollectPoint", "parent", "child", pipeline.StreamEdge, defaultEdgeBufferSize, ls)
+
+	e.Collect(point)
+	msg, ok := e.Emit()
 	if !ok {
 		t.Fatal("did not get point back out of edge")
 	}
-	if !reflect.DeepEqual(p, np) {
-		t.Errorf("unexpected point after passing through edge:\ngot:\n%v\nexp:\n%v\n", np, p)
+	if !reflect.DeepEqual(msg, point) {
+		t.Errorf("unexpected point after passing through edge:\ngot:\n%v\nexp:\n%v\n", msg, point)
 	}
 }
 
 func TestEdge_CollectBatch(t *testing.T) {
-	name := "test-collecte-batch"
 	ls := loggingtest.New()
-	now := time.Now()
-	e := NewLegacyEdge(newEdge("BCollectPoint", "parent", "child", pipeline.StreamEdge, defaultEdgeBufferSize, ls))
-	b := models.Batch{
-		Name:  name,
-		Group: models.ToGroupID(name, nil, models.Dimensions{}),
-		TMax:  now,
-		Tags: models.Tags{
-			"tag1": "value1",
-			"tag2": "value2",
-		},
-		Points: []models.BatchPoint{
-			{
-				Time: now.Add(-1 * time.Second),
-				Tags: models.Tags{
-					"tag1": "value1",
-					"tag2": "value2",
-					"tag3": "first",
-					"tag4": "first",
-				},
-				Fields: models.Fields{
-					"field1": 42,
-					"field2": 4.2,
-					"field3": 49,
-					"field4": 4.9,
-				},
-			},
-			{
-				Time: now,
-				Tags: models.Tags{
-					"tag1": "value1",
-					"tag2": "value2",
-					"tag3": "second",
-					"tag4": "second",
-				},
-				Fields: models.Fields{
-					"field1": 42,
-					"field2": 4.2,
-					"field3": 49,
-					"field4": 4.9,
-				},
-			},
-		},
-	}
-
-	e.CollectBatch(b)
-	nb, ok := e.NextBatch()
+	e := newEdge("TestEdge_CollectBatch", "parent", "child", pipeline.BatchEdge, defaultEdgeBufferSize, ls)
+	e.Collect(batch)
+	msg, ok := e.Emit()
 	if !ok {
 		t.Fatal("did not get batch back out of edge")
 	}
-	if !reflect.DeepEqual(b, nb) {
-		t.Errorf("unexpected batch after passing through edge:\ngot:\n%v\nexp:\n%v\n", nb, b)
+	if !reflect.DeepEqual(batch, msg) {
+		t.Errorf("unexpected batch after passing through edge:\ngot:\n%v\nexp:\n%v\n", msg, batch)
 	}
 }
 
+var emittedMsg edge.Message
+var emittedOK bool
+
 func BenchmarkCollectPoint(b *testing.B) {
-	name := "point"
 	b.ReportAllocs()
 	ls := loggingtest.New()
-	e := NewLegacyEdge(newEdge("BCollectPoint", "parent", "child", pipeline.StreamEdge, defaultEdgeBufferSize, ls))
-	p := models.Point{
-		Name: name,
-		Tags: models.Tags{
-			"tag1": "value1",
-			"tag2": "value2",
-			"tag3": "value3",
-			"tag4": "value4",
-		},
-		Group: models.ToGroupID(name, nil, models.Dimensions{}),
-		Fields: models.Fields{
-			"field1": 42,
-			"field2": 4.2,
-			"field3": 49,
-			"field4": 4.9,
-		},
-	}
-
+	e := newEdge("BenchmarkCollectPoint", "parent", "child", pipeline.StreamEdge, defaultEdgeBufferSize, ls)
 	b.ResetTimer()
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
-			e.CollectPoint(p)
-			e.NextPoint()
+			e.Collect(point)
+			emittedMsg, emittedOK = e.Emit()
+		}
+	})
+}
+
+func BenchmarkCollectBatch(b *testing.B) {
+	b.ReportAllocs()
+	ls := loggingtest.New()
+	e := newEdge("BCollectPoint", "parent", "child", pipeline.StreamEdge, defaultEdgeBufferSize, ls)
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			e.Collect(batch)
+			emittedMsg, emittedOK = e.Emit()
 		}
 	})
 }
