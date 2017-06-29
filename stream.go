5e1ee5c2debc3efa7df0dd3fd17ab6aa54cbcd0e
Nathaniel Cook <nvcook42@gmail.com>
2017-06-29 13:04:01 -0600

    update stream/from node

--- stream.go
+++ stream.go
@@ -1,9 +1,11 @@
 package kapacitor
 
 import (
+	"errors"
 	"fmt"
 	"log"
 
+	"github.com/influxdata/kapacitor/edge"
 	"github.com/influxdata/kapacitor/models"
 	"github.com/influxdata/kapacitor/pipeline"
 	"github.com/influxdata/kapacitor/tick/ast"
@@ -42,7 +44,7 @@ type FromNode struct {
 	s             *pipeline.FromNode
 	expression    stateful.Expression
 	scopePool     stateful.ScopePool
-	dimensions    []string
+	tagNames      []string
 	allDimensions bool
 	db            string
 	rp            string
@@ -59,7 +61,7 @@ func newFromNode(et *ExecutingTask, n *pipeline.FromNode, l *log.Logger) (*FromN
 		name: n.Measurement,
 	}
 	sn.node.runF = sn.runStream
-	sn.allDimensions, sn.dimensions = determineTagNames(n.Dimensions, nil)
+	sn.allDimensions, sn.tagNames = determineTagNames(n.Dimensions, nil)
 
 	if n.Lambda != nil {
 		expr, err := stateful.NewExpression(n.Lambda.Expression)
@@ -75,49 +77,59 @@ func newFromNode(et *ExecutingTask, n *pipeline.FromNode, l *log.Logger) (*FromN
 }
 
 func (s *FromNode) runStream([]byte) error {
-	ins := NewLegacyEdges(s.ins)
-	outs := NewLegacyEdges(s.outs)
-
-	dims := models.Dimensions{
-		ByName: s.s.GroupByMeasurementFlag,
-	}
-	for pt, ok := ins[0].NextPoint(); ok; pt, ok = ins[0].NextPoint() {
-		s.timer.Start()
-		if s.matches(pt) {
-			if s.s.Truncate != 0 {
-				pt.Time = pt.Time.Truncate(s.s.Truncate)
-			}
-			if s.s.Round != 0 {
-				pt.Time = pt.Time.Round(s.s.Round)
-			}
-			dims.TagNames = s.dimensions
-			pt = setGroupOnPoint(pt, s.allDimensions, dims, nil)
-			s.timer.Pause()
-			for _, child := range outs {
-				err := child.CollectPoint(pt)
-				if err != nil {
-					return err
-				}
-			}
-			s.timer.Resume()
-		}
-		s.timer.Stop()
-	}
-	return nil
+	consumer := edge.NewConsumerWithReceiver(
+		s.ins[0],
+		edge.NewReceiverFromForwardReceiverWithStats(
+			s.outs,
+			s,
+		),
+	)
+	return consumer.Consume()
+}
+func (s *FromNode) BeginBatch(edge.BeginBatchMessage) (edge.Message, error) {
+	return nil, errors.New("from does not support batch data")
+}
+func (s *FromNode) BatchPoint(edge.BatchPointMessage) (edge.Message, error) {
+	return nil, errors.New("from does not support batch data")
+}
+func (s *FromNode) EndBatch(edge.EndBatchMessage) (edge.Message, error) {
+	return nil, errors.New("from does not support batch data")
 }
 
-func (s *FromNode) matches(p models.Point) bool {
-	if s.db != "" && p.Database != s.db {
+func (s *FromNode) Point(p edge.PointMessage) (edge.Message, error) {
+	if s.matches(p) {
+		p = p.ShallowCopy()
+		if s.s.Truncate != 0 {
+			p.SetTime(p.Time().Truncate(s.s.Truncate))
+		}
+		if s.s.Round != 0 {
+			p.SetTime(p.Time().Round(s.s.Round))
+		}
+		p.SetDimensions(models.Dimensions{
+			ByName:   s.s.GroupByMeasurementFlag,
+			TagNames: computeTagNames(p.Tags(), s.allDimensions, s.tagNames, nil),
+		})
+		return p, nil
+	}
+	return nil, nil
+}
+
+func (s *FromNode) Barrier(b edge.BarrierMessage) (edge.Message, error) {
+	return b, nil
+}
+
+func (s *FromNode) matches(p edge.PointMessage) bool {
+	if s.db != "" && p.Database() != s.db {
 		return false
 	}
-	if s.rp != "" && p.RetentionPolicy != s.rp {
+	if s.rp != "" && p.RetentionPolicy() != s.rp {
 		return false
 	}
-	if s.name != "" && p.Name != s.name {
+	if s.name != "" && p.Name() != s.name {
 		return false
 	}
 	if s.expression != nil {
-		if pass, err := EvalPredicate(s.expression, s.scopePool, p.Time, p.Fields, p.Tags); err != nil {
+		if pass, err := EvalPredicate(s.expression, s.scopePool, p.Time(), p.Fields(), p.Tags()); err != nil {
 			s.incrementErrorCount()
 			s.logger.Println("E! error while evaluating WHERE expression:", err)
 			return false
