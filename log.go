6f3b76dca2a5c706ee0d2da1f5ad90b65c9abe3a
Nathaniel Cook <nvcook42@gmail.com>
2017-07-18 11:15:54 -0600

    update log node to use new edge messages

--- log.go
+++ log.go
@@ -7,14 +7,19 @@ import (
 	"log"
 	"strings"
 
+	"github.com/influxdata/kapacitor/edge"
 	"github.com/influxdata/kapacitor/pipeline"
 	"github.com/influxdata/wlog"
 )
 
 type LogNode struct {
 	node
-	level  wlog.Level
-	prefix string
+
+	key string
+	buf bytes.Buffer
+	enc *json.Encoder
+
+	batchBuffer *edge.BatchBuffer
 }
 
 // Create a new  LogNode which logs all data it receives
@@ -24,55 +29,61 @@ func newLogNode(et *ExecutingTask, n *pipeline.LogNode, l *log.Logger) (*LogNode
 		return nil, fmt.Errorf("invalid log level %s", n.Level)
 	}
 	nn := &LogNode{
-		node:   node{Node: n, et: et, logger: l},
-		level:  level,
-		prefix: n.Prefix,
+		node:        node{Node: n, et: et, logger: l},
+		key:         fmt.Sprintf("%c! %s", wlog.ReverseLevels[level], n.Prefix),
+		batchBuffer: new(edge.BatchBuffer),
 	}
+	nn.enc = json.NewEncoder(&nn.buf)
 	nn.node.runF = nn.runLog
 	return nn, nil
 }
 
 func (s *LogNode) runLog([]byte) error {
+	consumer := edge.NewConsumerWithReceiver(
+		s.ins[0],
+		edge.NewReceiverFromForwardReceiverWithStats(
+			s.outs,
+			edge.NewTimedForwardReceiver(s.timer, s),
+		),
+	)
+	return consumer.Consume()
 
-	ins := NewLegacyEdges(s.ins)
-	outs := NewLegacyEdges(s.outs)
+}
 
-	key := fmt.Sprintf("%c! %s", wlog.ReverseLevels[s.level], s.prefix)
-	var buf bytes.Buffer
-	env := json.NewEncoder(&buf)
-	switch s.Wants() {
-	case pipeline.StreamEdge:
-		for p, ok := ins[0].NextPoint(); ok; p, ok = ins[0].NextPoint() {
-			buf.Reset()
-			if err := env.Encode(p); err != nil {
-				s.incrementErrorCount()
-				s.logger.Println("E!", err)
-				continue
-			}
-			s.logger.Println(key, buf.String())
-			for _, child := range outs {
-				err := child.CollectPoint(p)
-				if err != nil {
-					return err
-				}
-			}
-		}
-	case pipeline.BatchEdge:
-		for b, ok := ins[0].NextBatch(); ok; b, ok = ins[0].NextBatch() {
-			buf.Reset()
-			if err := env.Encode(b); err != nil {
-				s.incrementErrorCount()
-				s.logger.Println("E!", err)
-				continue
-			}
-			s.logger.Println(key, buf.String())
-			for _, child := range outs {
-				err := child.CollectBatch(b)
-				if err != nil {
-					return err
-				}
-			}
-		}
+func (s *LogNode) BeginBatch(begin edge.BeginBatchMessage) (edge.Message, error) {
+	return nil, s.batchBuffer.BeginBatch(begin)
+}
+
+func (s *LogNode) BatchPoint(bp edge.BatchPointMessage) (edge.Message, error) {
+	return nil, s.batchBuffer.BatchPoint(bp)
+}
+
+func (s *LogNode) EndBatch(end edge.EndBatchMessage) (edge.Message, error) {
+	return s.BufferedBatch(s.batchBuffer.BufferedBatchMessage(end))
+}
+
+func (s *LogNode) BufferedBatch(batch edge.BufferedBatchMessage) (edge.Message, error) {
+	s.buf.Reset()
+	if err := s.enc.Encode(batch); err != nil {
+		s.incrementErrorCount()
+		s.logger.Println("E!", err)
+		return batch, nil
 	}
-	return nil
+	s.logger.Println(s.key, s.buf.String())
+	return batch, nil
+}
+
+func (s *LogNode) Point(p edge.PointMessage) (edge.Message, error) {
+	s.buf.Reset()
+	if err := s.enc.Encode(p); err != nil {
+		s.incrementErrorCount()
+		s.logger.Println("E!", err)
+		return p, nil
+	}
+	s.logger.Println(s.key, s.buf.String())
+	return p, nil
+}
+
+func (s *LogNode) Barrier(b edge.BarrierMessage) (edge.Message, error) {
+	return b, nil
 }
